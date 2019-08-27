ede282a6747da1e8b497ad74872a3579d9c98a61
monnaphon <51689788+monnaphon@users.noreply.github.com>
2019-08-27 11:31:32 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'remotemysql.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('eat43Q7WRE', 'forge'),
+            'username' => env('eat43Q7WRE', 'forge'),
+            'password' => env('hkzpgCtXqe', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
