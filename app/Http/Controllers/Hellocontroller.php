1c3a2acecacb50bcf7b96b38a944368885c49ebe
monnaphon <51689788+monnaphon@users.noreply.github.com>
2019-09-17 11:43:44 +0700

    Update Hellocontroller.php

--- app/Http/Controllers/Hellocontroller.php
+++ app/Http/Controllers/Hellocontroller.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+function showHello(){
+  return '<h1>Hello contreller</h1>'
+  }
+}
