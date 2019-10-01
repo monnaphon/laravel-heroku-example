90d621d52c9ab7eae5fc9a6a9e95141ef86b84d7
monnaphon <51689788+monnaphon@users.noreply.github.com>
2019-10-01 12:18:52 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HelloController extends Controller
 {
     function show(){
       return view('users')
-      ->with('name','monnaphon')
+      ->with('name','yo')
       ->with('title','Laravel tutorial');
   }
 }
