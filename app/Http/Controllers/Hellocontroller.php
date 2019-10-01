fb5e94d556a2fad757573310a32ea95046913dbf
monnaphon <51689788+monnaphon@users.noreply.github.com>
2019-10-01 11:14:07 +0700

    Update Hellocontroller.php

--- app/Http/Controllers/Hellocontroller.php
+++ app/Http/Controllers/Hellocontroller.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-function showHello($name){
+     function showHello($name){
   return '<h1>Hello:'.$name.'</h1>';
   }
  function resultProduct($product){
